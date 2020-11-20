3450421885945d5bf75ff5bd9eb3382cafcb7750
Elijah Cruz <elijah@ecwebservices.net>
2020-11-20 02:46:45 +0000

    Update

--- src/facades/Config.php
+++ src/facades/Config.php
@@ -5,13 +5,29 @@
 
 
     use Dotenv\Dotenv;
+    use RuntimeException;
 
     class Config
     {
-        public function __construct($directory){
-            $config = Dotenv::createImmutable($directory);
-            $config->load();
+        public $config;
 
-            return $config;
+        /**
+         * Config constructor.
+         * @param $directory
+         */
+        public function __construct($directory)
+        {
+            $config = Dotenv::createImmutable($directory);
+            $this->config = $config->load();
+        }
+
+        public function env($item) {
+            try{
+                $_ENV[$item];
+            }
+            catch (RuntimeException $e) {
+                echo 'Enviroment Variable Does Not Exist. \n';
+            }
+
         }
     }
