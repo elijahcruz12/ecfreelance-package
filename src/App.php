6cc14dec993d3b26d2382840c120e3313b8b2de8
Elijah Cruz <elijah@ecwebservices.net>
2020-11-20 02:05:42 +0000

    Update to the framework

--- src/App.php
+++ src/App.php
@@ -6,12 +6,13 @@ namespace ecwebservices\ecframework;
 
 class App
 {
+    public $directory;
     /**
      * Create a new Skeleton Instance
      */
-    public function __construct()
+    public function __construct(string $public)
     {
-        // constructor body
+        $this->directory = $public . '/../';
     }
 
     /**
