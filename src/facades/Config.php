719be30e15d86e161793f48fd28bd92804465f54
Elijah Cruz <elijah@ecwebservices.net>
2020-11-20 02:21:57 +0000

    Addded Config Facade

---
+++ src/facades/Config.php
@@ -0,0 +1,18 @@
+<?php
+
+
+    namespace ecwebservices\ecframework\facades;
+
+
+    use Dotenv\Dotenv;
+    use ecwebservices\ecframework\App;
+
+    class Config
+    {
+        public function init(){
+            $config = Dotenv::createImmutable(App::directory);
+            $config->load();
+
+            return $config;
+        }
+    }
